f135f8bc05ac3338be41c51bf154acb8701f54a2
iamasistiwari <iamashishtiwari2004@gmail.com>
2024-09-18 10:35:15 +0530

    initial commits 2

---
+++ webiste/apps/website/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Title } from "../../components/Title";
+import { TopBar } from "../../components/TopBar";
+
+
+
+
+export default function Page() {
+    return <div className="min-h-screen bg-gradient-to-b from-black to-gray-900">
+        <TopBar />
+        <Title />
+    </div>
+}
---
+++ webiste/apps/website/components/Title.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+export const Title = () => {
+    return <div className="text-white font-mulish flex flex-col justify-center mt-24 items-center">
+        <div className="text-white flex justify-center font-extrabold text-5xl">
+            AI Chat
+        </div>
+        <div className="flex justify-center font-mulish font-semibold max-w-3xl pl-5 text-lg pt-10">
+            AI Chat is an AI chatbot that writes text. You can use it to write stories, messages, or programming code. You can use the AI chatbot as a virtual tutor in almost any subject.
+        </div>
+    </div>
+}

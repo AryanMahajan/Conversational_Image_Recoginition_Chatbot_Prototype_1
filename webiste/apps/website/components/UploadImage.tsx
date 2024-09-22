1b3582358d3b8f84455b65f02d7a3343de7c2b0d
iamasistiwari <iamashishtiwari2004@gmail.com>
2024-09-22 23:40:31 +0530

    image-upload refined

--- webiste/apps/website/components/UploadImage.tsx
+++ webiste/apps/website/components/UploadImage.tsx
@@ -39,13 +39,12 @@ export const UploadImage = () => {
                 }}
                 onUploadError={handleUploadError}
                 
-            /> : <div className="mt-2">
+            /> : <div className="mt-2 w-[720px]">
                     <ul>
                         {images.map((image, index) => (
                             <li key={index} className="mt-2 text-white">
-                                <p><strong>File Key:</strong> {image.fileKey}</p>
-                                <p><strong>File Name:</strong> {image.fileName}</p>
-                                <Image src={image.fileKey} alt={image.fileName} width={200} height={200} />
+                                <p><strong>File Key: </strong>{image.fileKey}</p>
+                                <ImageLoader src={image.fileKey} alt={image.fileName} />
                             </li>
                         ))}
                     </ul>
@@ -54,3 +53,32 @@ export const UploadImage = () => {
         </main>
     );
 }
+
+
+const ImageLoader = ({ src, alt }: {src: string, alt: string}) => {
+    const [loading, setLoading] = useState(true);
+
+    const handleLoad = () => {
+        setTimeout(() => {
+            setLoading(false);
+        },5000)
+    };
+    return (
+        <div className="relative">
+            {loading && (
+                <div
+                    className="absolute inline-block h-8 mt-32 ml-32 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+                    role="status">
+                    <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">Loading...</span>
+                </div>
+            )}
+            <Image className="absolute inset-0 flex items-center mt-4 justify-center rounded-lg border-2 border-white w-[300]"
+                src={src}
+                alt={alt}
+                width={300}
+                height={300}
+                onLoad={handleLoad}
+            />
+        </div>
+    );
+};

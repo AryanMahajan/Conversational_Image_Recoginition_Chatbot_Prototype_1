e0e8ffaf35a6ac913f99f3c37fd2796fdbd29c5a
AryanMahajan <mahajanaryan0409@gmail.com>
2024-09-24 07:16:27 +0530

    xml_parsing_train

--- models/image_recoginiton/datapreprocessing.py
+++ models/image_recoginiton/datapreprocessing.py
@@ -24,8 +24,6 @@ def get_class_from_annotation(img_name,category):
         tree = ET.parse(os.path.join(ann_path, f"{img_name}.xml"))
         root = tree.getroot()
 
-        print("Root:", root)
-
         for object in root.findall('object'):
             name = object.find('name').text
             class_categories.append(name)
@@ -36,6 +34,32 @@ def get_class_from_annotation(img_name,category):
     
     return class_categories
 
+def get_bbox_coordinates(img_name, category):
+    coordinates = []
+    categories = ["train_val","test"]
+    if category == categories[0]:
+        ann_path = TRAIN_IMAGES_ANNOTATIONS
+    else:
+        ann_path = TEST_IMAGES_ANNOTATIONS
+
+    try:
+        tree = ET.parse(os.path.join(ann_path, f"{img_name}.xml"))
+        root = tree.getroot()
+        coordinates = []
+
+        for object in root.findall('object'):
+            bbox = object.find('bndbox')
+            xmin = int(bbox.find('xmin').text)
+            ymin = int(bbox.find('ymin').text)
+            xmax = int(bbox.find('xmax').text)
+            ymax = int(bbox.find('ymax').text)
+            coordinates.append([xmin, ymin, xmax, ymax])
+
+        return coordinates 
+    except Exception as e:
+        print(f"Error parsing XML for image {img_name}: {e}")
+        return []  # Return an empty list in case of errors
+
 def create_train_val_dataset():
     # Define the path to the dataset
     img_path = TRAIN_IMAGES
@@ -43,7 +67,7 @@ def create_train_val_dataset():
         try:
             img_array = cv2.imread(os.path.join(img_path,img) ,cv2.IMREAD_GRAYSCALE)  # convert to array
             new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))  # resize to normalize data size
-            training_data.append([new_array, get_class_from_annotation(img[:-5], "train_val")])  # add this to our training_data
+            training_data.append([new_array, get_class_from_annotation(img[:-5], "train_val"), get_bbox_coordinates(img[:-5], "train_val")])  # add this to our training_data
             print(get_class_from_annotation(img[:-5],"train_val"))
         except Exception as e:  # in the interest in keeping the output clean...
             pass
